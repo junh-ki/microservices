74c842662590a74147b0e5719a2762bb9d38fa9b
Junhyung Ki <kijoonh91@gmail.com>
2022-04-29 18:29:04 +0200

    reformat product-service

--- microservices/product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
@@ -8,7 +8,8 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("se.magnus")
 public class ProductServiceApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(ProductServiceApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
 }
