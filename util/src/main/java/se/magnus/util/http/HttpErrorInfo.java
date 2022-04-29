05b5ffbcb75573db1d7b34b9cfb6125a31e75629
Junhyung Ki <kijoonh91@gmail.com>
2022-04-29 19:01:22 +0200

    reformat util

--- util/src/main/java/se/magnus/util/http/HttpErrorInfo.java
+++ util/src/main/java/se/magnus/util/http/HttpErrorInfo.java
@@ -1,45 +1,47 @@
 package se.magnus.util.http;
 
 import java.time.ZonedDateTime;
+
 import org.springframework.http.HttpStatus;
 
 public class HttpErrorInfo {
-  private final ZonedDateTime timestamp;
-  private final String path;
-  private final HttpStatus httpStatus;
-  private final String message;
+    private final ZonedDateTime timestamp;
+    private final String path;
+    private final HttpStatus httpStatus;
+    private final String message;
 
-  public HttpErrorInfo() {
-    timestamp = null;
-    this.httpStatus = null;
-    this.path = null;
-    this.message = null;
-  }
+    public HttpErrorInfo() {
+        this.timestamp = null;
+        this.httpStatus = null;
+        this.path = null;
+        this.message = null;
+    }
 
-  public HttpErrorInfo(HttpStatus httpStatus, String path, String message) {
-    timestamp = ZonedDateTime.now();
-    this.httpStatus = httpStatus;
-    this.path = path;
-    this.message = message;
-  }
+    public HttpErrorInfo(HttpStatus httpStatus, String path, String message) {
+        this.timestamp = ZonedDateTime.now();
+        this.httpStatus = httpStatus;
+        this.path = path;
+        this.message = message;
+    }
 
-  public ZonedDateTime getTimestamp() {
-    return timestamp;
-  }
+    public ZonedDateTime getTimestamp() {
+        return this.timestamp;
+    }
 
-  public String getPath() {
-    return path;
-  }
+    public String getPath() {
+        return this.path;
+    }
 
-  public int getStatus() {
-    return httpStatus.value();
-  }
+    public int getStatus() {
+        return this.httpStatus.value();
+    }
 
-  public String getError() {
-    return httpStatus.getReasonPhrase();
-  }
+    public String getError() {
+        return this.httpStatus.getReasonPhrase();
+    }
 
-  public String getMessage() {
-    return message;
-  }
+    public String getMessage() {
+        return this.message;
+    }
+
 }
