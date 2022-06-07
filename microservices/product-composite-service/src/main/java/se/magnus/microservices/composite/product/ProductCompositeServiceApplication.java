f74095d0fcc8c52ac8519975173fc87129116f5d
Junhyung Ki <kijoonh91@gmail.com>
2022-06-07 12:09:46 +0900

    Add OpenAPI configuration and general API documentation to the ProductCompositeService

--- microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -1,5 +1,11 @@
 package se.magnus.microservices.composite.product;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,9 +16,40 @@ import org.springframework.web.client.RestTemplate;
 @ComponentScan("se.magnus")
 public class ProductCompositeServiceApplication {
 
+    @Value("${api.common.version}")         String apiVersion;
+    @Value("${api.common.title}")           String apiTitle;
+    @Value("${api.common.description}")     String apiDescription;
+    @Value("${api.common.termsOfService}")  String apiTermsOfService;
+    @Value("${api.common.license}")         String apiLicense;
+    @Value("${api.common.licenseUrl}")      String apiLicenseUrl;
+    @Value("${api.common.externalDocDesc}") String apiExternalDocDesc;
+    @Value("${api.common.externalDocUrl}")  String apiExternalDocUrl;
+    @Value("${api.common.contact.name}")    String apiContactName;
+    @Value("${api.common.contact.url}")     String apiContactUrl;
+    @Value("${api.common.contact.email}")   String apiContactEmail;
+
     @Bean
     RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public OpenAPI getOpenApiDocumentation() {
+        return new OpenAPI()
+                .info(new Info().title(this.apiTitle)
+                        .description(this.apiDescription)
+                        .version(this.apiVersion)
+                        .contact(new Contact()
+                                .name(this.apiContactName)
+                                .url(this.apiContactUrl)
+                                .email(this.apiContactEmail))
+                        .termsOfService(this.apiTermsOfService)
+                        .license(new License()
+                                .name(this.apiLicense)
+                                .url(this.apiLicenseUrl)))
+                .externalDocs(new ExternalDocumentation()
+                        .description(this.apiExternalDocDesc)
+                        .url(this.apiExternalDocUrl));
     }
 
     public static void main(String[] args) {
