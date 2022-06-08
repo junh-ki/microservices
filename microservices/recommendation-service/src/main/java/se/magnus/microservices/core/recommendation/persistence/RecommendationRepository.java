2599274141156d095c6d2e18cf1e658a18988860
Junhyung Ki <kijoonh91@gmail.com>
2022-06-08 13:13:08 +0900

    Define repositories for Product, Recommendation and Review services

--- microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/persistence/RecommendationRepository.java
+++ microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/persistence/RecommendationRepository.java
@@ -1,2 +1,10 @@
-package se.magnus.microservices.core.recommendation.persistence;public class RecommendationRepository {
+package se.magnus.microservices.core.recommendation.persistence;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecommendationRepository extends CrudRepository<RecommendationEntity, String> {
+
+    List<RecommendationEntity> findByProductId(int productId);
+
 }
