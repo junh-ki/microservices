f7c17f697bfe2f82f89ff08e2e8f6d0e389a519f
Junhyung Ki <kijoonh91@gmail.com>
2022-06-10 08:39:23 +0900

    Log the database connection URL

--- microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/RecommendationServiceApplication.java
+++ microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/RecommendationServiceApplication.java
@@ -1,15 +1,48 @@
 package se.magnus.microservices.core.recommendation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.mapping.context.MappingContext;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.index.IndexOperations;
+import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
+import org.springframework.data.mongodb.core.mapping.MongoPersistentEntity;
+import org.springframework.data.mongodb.core.mapping.MongoPersistentProperty;
+import se.magnus.microservices.core.recommendation.persistence.RecommendationEntity;
 
 @SpringBootApplication
 @ComponentScan("se.magnus")
 public class RecommendationServiceApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(RecommendationServiceApplication.class);
+    private final MongoOperations mongoTemplate;
+
+    @Autowired
+    public RecommendationServiceApplication(MongoOperations mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(RecommendationServiceApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(RecommendationServiceApplication.class, args);
+        String mongoDbHost = ctx.getEnvironment().getProperty("spring.data.mongodb.host");
+        String mongoDbPort = ctx.getEnvironment().getProperty("spring.data.mongodb.port");
+        LOG.info("Connected to MongoDb: " + mongoDbHost + ":" + mongoDbPort);
+    }
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void initIndicesAfterStartup() {
+        MappingContext<? extends MongoPersistentEntity<?>, MongoPersistentProperty> mappingContext = mongoTemplate.getConverter().getMappingContext();
+        MongoPersistentEntityIndexResolver mongoPersistentEntityIndexResolver = new MongoPersistentEntityIndexResolver(mappingContext);
+        IndexOperations indexOps = mongoTemplate.indexOps(RecommendationEntity.class);
+        mongoPersistentEntityIndexResolver.resolveIndexFor(RecommendationEntity.class)
+                .forEach(indexOps::ensureIndex);
     }
 
 }
